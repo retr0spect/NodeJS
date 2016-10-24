efc6615728c5dd69672500fd5861e8d2b55db06f
Aditya <aditya.chandel@aggiemail.usu.edu>
2016-10-23 23:34:30 -0600

    Event Emitter

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Aditya on 10/23/2016.
+ */
+
+var Emitter = require('./emitter');
+
+var emtr = new Emitter();
+
+emtr.on('greet', function () {
+    console.log('Somewhere, someone said hello!');
+});
+
+emtr.on('greet', function () {
+    console.log('A greeting occurred!');
+});
+
+console.log('Hello!');
+
+emtr.emit('greet');
---
+++ emitter.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Aditya on 10/23/2016.
+ */
+
+function Emitter() {
+    this.events = {};
+}
+
+Emitter.prototype.on = function (type, listener) {
+    this.events[type] = this.events[type] || [];
+    this.events[type].push(listener);
+};
+
+Emitter.prototype.emit = function (type) {
+    if (this.events[type]) {
+        this.events[type].forEach(function (listener) {
+            listener();
+        });
+    }
+};
+
+module.exports = Emitter;
